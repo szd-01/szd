7823af761160da41584403820d4c5c84223ab65a
szd <sunzhenduo01@gmail.com>
2020-12-31 16:36:21 +0800

    xxx

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,6 +4,8 @@ public class Test {
     int i;
 
     public static void main(String[] args) {
-        
+        System.out.println("1231");
+        System.out.println("asdasd");
+        System.out.println("asdasd");
     }
 }
